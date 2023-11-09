d1a5984e31772f1433099f233f300a5c14305fd9
opielapatryk <xpatrykopiela@gmail.com>
2023-11-09 18:00:19 +0100

    experimenting with navigation

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { styles } from './styles/styles';
-import { MovieSearchPage } from './components/MovieSearchPage';
+import Example from './components/Switch'
+export default Example
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MovieSearchPage/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// import { StatusBar } from 'expo-status-bar';
+// import { View } from 'react-native';
+// import { styles } from './styles/styles';
+// import { MovieSearchPage } from './components/MovieSearchPage';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <MovieSearchPage/>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
