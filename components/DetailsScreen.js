ae628b5974edabfae35a51f38e48cc3ab283bfed
opielapatryk <xpatrykopiela@gmail.com>
2023-11-10 11:54:30 +0100

    all done

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native';
+import { movie as movieList } from '../mockData';
 
-export function DetailsScreen(){
-    return (
-      <View>
-        <Text>Hello from details screen</Text>
-      </View>
-    )
-  }
+export function DetailsScreen({ route }) {
+  const { movie } = route.params;
+
+  // Check if the movie's imdbID matches the one in movieList
+  const isMovieFound = movieList['imdbID'] === movie['imdbID'];
+
+  return (
+    <View>
+      <Text>Hello from details screen</Text>
+      {isMovieFound ? (
+        // If movie is found, iterate over the keys of movieList
+        Object.keys(movieList).map((key) => (
+          <View key={key}>
+            {key === 'Ratings' ? (
+              // If the key is 'Ratings', iterate over the array of ratings
+              <View>
+                {movieList[key].map((rating, index) => (
+                  <Text key={index}>
+                    {rating['Source']}: {rating['Value']}
+                  </Text>
+                ))}
+              </View>
+            ) : (
+              // If the key is not 'Ratings', display the key-value pair
+              <Text>
+                {key}: {movieList[key]}
+              </Text>
+            )}
+          </View>
+        ))
+      ) : (
+        <Text>Movie not found</Text>
+      )}
+    </View>
+  );
+}
--- components/MovieSearchPage.js
+++ components/MovieSearchPage.js
@@ -1,7 +1,7 @@
 import { Text, TextInput, View, Button, ScrollView } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import { styles } from '../styles/styles';
-import {search, movie} from '../mockData';
+import {search} from '../mockData';
 
 export function MovieSearchPage({ navigation }){
   const [movieTitle, setMovieTitle] = useState('');
@@ -11,7 +11,9 @@ export function MovieSearchPage({ navigation }){
       <Text>Search for any movie to see more data</Text>
       <TextInput placeholder='Title' style={styles.input} onChangeText={newMovieTitle=>setMovieTitle(newMovieTitle)}/>
       <ScrollView>
-        {search['Search'].filter((item)=> item.Title.toLowerCase().includes(movieTitle.toLowerCase())).map((item) => <Button onPress={() => navigation.push('Movie Details')} key={item['imdbID']} title={item['Title']}/>)}
+        {search['Search'].filter((item)=> item.Title.toLowerCase().includes(movieTitle.toLowerCase())).map((item) => <Button onPress={() => navigation.push('Movie Details', {
+            movie: item,
+          })} key={item['imdbID']} title={item['Title']}/>)}
       </ScrollView>
     </View>
   );
