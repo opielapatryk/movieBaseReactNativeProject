eaf099e099d318d19bfdb16c6804862783d3c6e0
opielapatryk <xpatrykopiela@gmail.com>
2023-11-10 13:20:32 +0100

    updated with API

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,37 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { movie as movieList } from '../mockData';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image } from 'react-native';
 
 export function DetailsScreen({ route }) {
   const { movie } = route.params;
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [isMovieFound, setIsMovieFound] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // Check if the movie's imdbID matches the one in movieList
-  const isMovieFound = movieList['imdbID'] === movie['imdbID'];
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?apikey=b32be2df&t=${movie['Title']}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.Response === 'True') {
+          setMovieDetails(data);
+        } else {
+          setIsMovieFound(false);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching movie details:', error);
+        setIsMovieFound(false);
+      })
+      .finally(() => setIsLoading(false));
+  }, [movie]);
 
   return (
     <View>
       <Text>Hello from details screen</Text>
-      {isMovieFound ? (
-        // If movie is found, iterate over the keys of movieList
-        Object.keys(movieList).map((key) => (
-          <View key={key}>
-            {key === 'Ratings' ? (
-              // If the key is 'Ratings', iterate over the array of ratings
-              <View>
-                {movieList[key].map((rating, index) => (
-                  <Text key={index}>
-                    {rating['Source']}: {rating['Value']}
-                  </Text>
-                ))}
-              </View>
-            ) : (
-              // If the key is not 'Ratings', display the key-value pair
-              <Text>
-                {key}: {movieList[key]}
-              </Text>
-            )}
-          </View>
-        ))
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : isMovieFound ? (
+        <View>
+          <Image
+            source={{ uri: movieDetails.Poster }}
+            style={{ width: 200, height: 300 }}
+          />
+          <Text>Title: {movieDetails.Title}</Text>
+          <Text>Year: {movieDetails.Year}</Text>
+          <Text>Rated: {movieDetails.Rated}</Text>
+          <Text>Released: {movieDetails.Released}</Text>
+          <Text>Runtime: {movieDetails.Runtime}</Text>
+          <Text>Genre: {movieDetails.Genre}</Text>
+          <Text>Director: {movieDetails.Director}</Text>
+          <Text>Writer: {movieDetails.Writer}</Text>
+          <Text>Actors: {movieDetails.Actors}</Text>
+          <Text>Plot: {movieDetails.Plot}</Text>
+          <Text>Language: {movieDetails.Language}</Text>
+          <Text>Country: {movieDetails.Country}</Text>
+          <Text>Awards: {movieDetails.Awards}</Text>
+          <Text>Metascore: {movieDetails.Metascore}</Text>
+          <Text>IMDb Rating: {movieDetails.imdbRating}</Text>
+          <Text>IMDb Votes: {movieDetails.imdbVotes}</Text>
+          <Text>IMDb ID: {movieDetails.imdbID}</Text>
+          <Text>Type: {movieDetails.Type}</Text>
+          <Text>DVD: {movieDetails.DVD}</Text>
+          <Text>Box Office: {movieDetails.BoxOffice}</Text>
+          <Text>Production: {movieDetails.Production}</Text>
+          <Text>Website: {movieDetails.Website}</Text>
+        </View>
       ) : (
         <Text>Movie not found</Text>
       )}
--- components/MovieSearchPage.js
+++ components/MovieSearchPage.js
@@ -1,20 +1,56 @@
-import { Text, TextInput, View, Button, ScrollView } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
+import { Text, TextInput, View, Button, ScrollView, ActivityIndicator } from 'react-native';
 import { styles } from '../styles/styles';
-import {search} from '../mockData';
 
-export function MovieSearchPage({ navigation }){
+export function MovieSearchPage({ navigation }) {
   const [movieTitle, setMovieTitle] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchMovies() {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`https://www.omdbapi.com/?apikey=b32be2df&s=${movieTitle}`);
+      const result = await response.json();
+
+      if (result.Response === 'True') {
+        setSearchResults(result.Search);
+      } else {
+        setSearchResults([]);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Welcome in Movie Browser!</Text>
+      <Text>Welcome to Movie Browser!</Text>
       <Text>Search for any movie to see more data</Text>
-      <TextInput placeholder='Title' style={styles.input} onChangeText={newMovieTitle=>setMovieTitle(newMovieTitle)}/>
+      <TextInput
+        placeholder="Title"
+        style={styles.input}
+        onChangeText={(newMovieTitle) => setMovieTitle(newMovieTitle)}
+      />
+      <Button title="Search" onPress={fetchMovies} />
       <ScrollView>
-        {search['Search'].filter((item)=> item.Title.toLowerCase().includes(movieTitle.toLowerCase())).map((item) => <Button onPress={() => navigation.push('Movie Details', {
-            movie: item,
-          })} key={item['imdbID']} title={item['Title']}/>)}
+        {isLoading && <ActivityIndicator size="large" color="#0000ff" />}
+        {!isLoading &&
+          searchResults.map((item) => (
+            <Button
+              onPress={() =>
+                navigation.push('Movie Details', {
+                  movie: item,
+                })
+              }
+              key={item['imdbID']}
+              title={item['Title']}
+            />
+          ))}
+        {!isLoading && searchResults.length === 0 && <Text>No results found</Text>}
       </ScrollView>
     </View>
   );
-};
+}
